deb24731fcedcb4094315a4d7881fee172ce5035
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2025-08-03 08:06:27 +0200

    Welcome szöveg javítása

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import streamlit as st
 
-st.title("Hello Streamlit-er 👋")
+st.title("Hello 👋")
 st.markdown(
     """ 
-    This is a playground for you to try Streamlit and have fun. 
-
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
+   Welcome to Andrea's Streamlit app!
     """
 )
 
