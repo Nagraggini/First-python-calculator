2db0330a67e98806202b8198f87910702965dcb6
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2025-08-10 21:53:05 +0200

    comments

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-#Terminalban:
-#cd "C:\Users\Asus\Documents\Visual Studio Code\First-python-project"
-#flask run vagy python app.py
-#localhost: 127.0.0.1:5000/
+'''Terminalban:
+cd "C:\Users\Asus\Documents\Visual Studio Code\First-python-project"
+flask run vagy python app.py
+localhost: 127.0.0.1:5000/'''
+
+#Rövid komment.
 
 from flask import Flask, render_template, request
 
@@ -10,3 +12,4 @@ app = Flask(__name__) # Flask alkalmazás létrehozása
 @app.route("/")
 def home():
     return render_template("calculator.html")
+
